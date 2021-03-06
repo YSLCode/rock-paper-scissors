a9560c07c78a82ea58d0457feefdbc3e84e35ca4
YSLCode <YSLCode@users.noreply.github.com>
2021-03-06 20:14:02 +0330

    checking if score cookie exists in the first time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import rules from "./rules.js";
 import eventActivate from "./events.js";
-
+import { setCookie, getCookie } from "./cookies.js";
+document.getElementById("score-number").innerHTML = getCookie("userScore") ?
+    getCookie("userScore") :
+    "--";
 rules();
 eventActivate();
